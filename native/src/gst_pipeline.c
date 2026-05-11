5adaeefbc1ed86de93344f4d6ce978a62f730fb5
Eldy Reynanda Baghaskara <eldy.baghaskara@vidio.com>
2026-05-11 18:38:43 +0700

    fix(sdi): add dynamic caps mapping for upscaling based on video_mode

--- native/src/gst_pipeline.c
+++ native/src/gst_pipeline.c
@@ -1277,7 +1277,43 @@ gboolean add_sink_to_pipeline(GstElement *pipeline, GstElement *tee, cJSON *sink
         }
 
         // --- Configure caps for DeckLink ---
-        GstCaps *caps = gst_caps_from_string("video/x-raw, format=UYVY");
+        char caps_str[128];
+        strcpy(caps_str, "video/x-raw, format=UYVY");
+
+        switch (video_mode) {
+            case 1: // 1080p 25fps (PAL)
+                strcat(caps_str, ", width=1920, height=1080, framerate=25/1");
+                break;
+            case 2: // 1080p 30fps (NTSC)
+                strcat(caps_str, ", width=1920, height=1080, framerate=30/1");
+                break;
+            case 3: // 1080p 50fps
+                strcat(caps_str, ", width=1920, height=1080, framerate=50/1");
+                break;
+            case 4: // 1080p 60fps
+                strcat(caps_str, ", width=1920, height=1080, framerate=60/1");
+                break;
+            case 5: // 1080i 50fps (PAL)
+                strcat(caps_str, ", width=1920, height=1080, framerate=25/1"); // field rate 50
+                break;
+            case 6: // 1080i 60fps (NTSC)
+                strcat(caps_str, ", width=1920, height=1080, framerate=30/1"); // field rate 60
+                break;
+            case 7: // 720p 50fps
+                strcat(caps_str, ", width=1280, height=720, framerate=50/1");
+                break;
+            case 8: // 720p 60fps
+                strcat(caps_str, ", width=1280, height=720, framerate=60/1");
+                break;
+            default:
+                // Default to 1080p25 if unknown or Auto
+                strcat(caps_str, ", width=1920, height=1080, framerate=25/1");
+                break;
+        }
+
+        g_print("SDI sink: video_mode=%d -> caps: %s\n", video_mode, caps_str);
+
+        GstCaps *caps = gst_caps_from_string(caps_str);
         g_object_set(vcaps, "caps", caps, NULL);
         gst_caps_unref(caps);
 
