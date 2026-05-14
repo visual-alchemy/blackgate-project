dd7f100dbe522e40bd9d191a925c0a4e89454acb
Eldy Reynanda Baghaskara <eldy.baghaskara@vidio.com>
2026-05-14 19:22:09 +0700

    fix: add -mpegts_copyts 1 -pcr_period 40 to ffmpeg sidecar
    Preserves original timestamps and forces regular PCR intervals in the
    MPEG-TS output for more consistent timing to GStreamer.

--- lib/blackgate/route_handler.ex
+++ lib/blackgate/route_handler.ex
@@ -485,7 +485,7 @@ defmodule Blackgate.RouteHandler do
     "ffmpeg -hide_banner -loglevel warning " <>
       "#{reconnect_flags} " <>
       "-i \"#{url}\" " <>
-      "-c copy -f mpegts " <>
+      "-c copy -mpegts_copyts 1 -pcr_period 40 -f mpegts " <>
       "\"srt://127.0.0.1:#{internal_port}?mode=listener&latency=500\""
   end
 
